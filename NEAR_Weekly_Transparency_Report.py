32ae5a81dae876cb58b1456a000888ab1e580e47
Adrià Parcerisas <adria.parcerisas.albes@gmail.com>
2023-01-23 12:12:13 +0100

    avoiding cache issues
    Solving problems loading queries with cache

--- NEAR_Weekly_Transparency_Report.py
+++ NEAR_Weekly_Transparency_Report.py
@@ -297,9 +297,10 @@ ORDER BY date DESC
 
 
 st.experimental_memo(ttl=21600)
+@st.cache
 def compute(a):
-    data=sdk.query(a)
-    return data
+    results=sdk.query(a)
+    return results
 
 results = compute(sql)
 df = pd.DataFrame(results.records)
